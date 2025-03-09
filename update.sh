a38d95393b0572798f0b5b059e6fb81d937019eb
BL00DKNIGHT <kauanhenrique29781054@gmail.com>
2025-03-09 17:02:22 -0300

    modified:   update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 printf "\e[34m[$0]: UPDATING CONFIGS\n"
 git pull
-rsync -avh ./config/hyprwal/* $HOME/.config/
-rsync -avh ./extra/librewolf/librewolf.overrides.cfg $HOME/.librewolf
+rsync -ah ./config/hyprwal/* $HOME/.config/
+rsync -ah ./extra/librewolf/librewolf.overrides.cfg $HOME/.librewolf
 printf "  DONE."
