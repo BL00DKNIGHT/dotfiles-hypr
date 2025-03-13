94b33c70dd4f54082e80bed2493ef8ca5c50509d
BL00DKNIGHT <kauanhenrique29781054@gmail.com>
2025-03-13 10:53:47 -0300

    unifying the setup and update script

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-printf "\e[34m[$0]: UPDATING CONFIGS\n"
-git pull
-rsync -ah ./config/hyprwal/* $HOME/.config/
-rsync -ah ./extra/librewolf/librewolf.overrides.cfg $HOME/.librewolf
-printf "  DONE."
